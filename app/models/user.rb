b44552545547cc32f87b87a1766e2c3d2a611e22
VanshiJodhani <vanshijodhani27@gmail.com>
2022-06-30 12:03:20 +0530

    model files

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :lockable
+
+  belongs_to :role
 end
